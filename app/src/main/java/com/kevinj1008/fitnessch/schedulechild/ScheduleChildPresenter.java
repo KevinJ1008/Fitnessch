4f088857850a3e92359843703738d8eac653e1a1
KevinJ1008 <car4353666@gmail.com>
2018-10-19 19:31:00 +0800

    Modified profile loading progress

--- app/src/main/java/com/kevinj1008/fitnessch/schedulechild/ScheduleChildPresenter.java
+++ app/src/main/java/com/kevinj1008/fitnessch/schedulechild/ScheduleChildPresenter.java
@@ -47,60 +47,57 @@ public class ScheduleChildPresenter implements ScheduleChildContract.Presenter{
 
     @Override
     public void loadArticles() {
-        if (!isLoading()) {
+        //TODO: Make author to user ID
+        String author = mSharedPreferencesManager.getUserDbUid();
+        //
 
-            //TODO: Make author to user ID
-            String author = mSharedPreferencesManager.getUserDbUid();
-            //
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        db.collection("articles")
+                .orderBy("create_time", Query.Direction.ASCENDING)
+                .whereEqualTo("user_id", author)
+                .whereEqualTo("article_tag", "課表")
+                .addSnapshotListener(new EventListener<QuerySnapshot>() {
+                    @Override
+                    public void onEvent(@Nullable QuerySnapshot snapshot, @Nullable FirebaseFirestoreException e) {
+                        if (e != null) {
+                            Log.d(Constants.TAG, "Articles listen failed.", e);
+                            return;
+                        }
+                        String source = snapshot != null && snapshot.getMetadata().hasPendingWrites() ? "Local" : "Server";
+                        if (source.equals("Server")) {
+                            for (DocumentChange documentChange  : snapshot.getDocumentChanges()) {
+                                Log.d(Constants.TAG, "Get Articles " + documentChange.toString());
+                                String id = documentChange.getDocument().getId();
+                                String author = documentChange.getDocument().getData().get("author").toString();
+                                String authorId = documentChange.getDocument().getData().get("user_id").toString();
 
-            setLoading(true);
-            FirebaseFirestore db = FirebaseFirestore.getInstance();
-            db.collection("articles")
-                    .orderBy("create_time", Query.Direction.ASCENDING)
-                    .whereEqualTo("user_id", author)
-                    .whereEqualTo("article_tag", "課表")
-                    .addSnapshotListener(new EventListener<QuerySnapshot>() {
-                        @Override
-                        public void onEvent(@Nullable QuerySnapshot snapshot, @Nullable FirebaseFirestoreException e) {
-                            if (e != null) {
-                                Log.d(Constants.TAG, "Articles listen failed.", e);
-                                return;
-                            }
-                            String source = snapshot != null && snapshot.getMetadata().hasPendingWrites() ? "Local" : "Server";
-                            if (source.equals("Server")) {
-                                for (DocumentChange documentChange  : snapshot.getDocumentChanges()) {
-                                    Log.d(Constants.TAG, "Get Articles " + documentChange.toString());
-                                    String id = documentChange.getDocument().getId();
-                                    String author = documentChange.getDocument().getData().get("author").toString();
-                                    String authorId = documentChange.getDocument().getData().get("user_id").toString();
+                                //TODO: Add author photo
+                                String authorPhoto = documentChange.getDocument().getData().get("author_photo").toString();
 
-                                    //TODO: Add author photo
-                                    String authorPhoto = documentChange.getDocument().getData().get("author_photo").toString();
+                                String title = documentChange.getDocument().getData().get("title").toString();
+                                String content = documentChange.getDocument().getData().get("content").toString();
+                                String time = String.valueOf(documentChange.getDocument().getTimestamp("create_time").getSeconds());
+                                int createTime = Integer.parseInt(time);
+                                String tag = documentChange.getDocument().getData().get("article_tag").toString();
 
-                                    String title = documentChange.getDocument().getData().get("title").toString();
-                                    String content = documentChange.getDocument().getData().get("content").toString();
-                                    String time = String.valueOf(documentChange.getDocument().getTimestamp("create_time").getSeconds());
-                                    int createTime = Integer.parseInt(time);
-                                    String tag = documentChange.getDocument().getData().get("article_tag").toString();
+                                Article articles = new Article();
 
-                                    Article articles = new Article();
+                                //TODO: Add author ID and photo to object
+                                articles.setId(id);
+                                    articles.setName(author);
+                                        articles.setTitle(title);
+                                            articles.setContent(content);
+                                            articles.setCreatedTime(createTime);
+                                        articles.setTag(tag);
+                                    articles.setAuthorId(authorId);
+                                articles.setAuthorImage(authorPhoto);
 
-                                    //TODO: Add author ID and photo to object
-                                    articles.setId(id);
-                                        articles.setName(author);
-                                            articles.setTitle(title);
-                                                articles.setContent(content);
-                                                articles.setCreatedTime(createTime);
-                                            articles.setTag(tag);
-                                        articles.setAuthorId(authorId);
-                                    articles.setAuthorImage(authorPhoto);
-
-                                    mScheduleChildView.showArticles(articles);
-                                }
+                                mScheduleChildView.showArticles(articles);
                             }
                         }
-                    });
-        }
+                    }
+                });
+
     }
 
     @Override
@@ -112,7 +109,7 @@ public class ScheduleChildPresenter implements ScheduleChildContract.Presenter{
     public void onScrollStateChanged(int visibleItemCount, int totalItemCount, int newState) {
         if (newState == RecyclerView.SCROLL_STATE_IDLE && visibleItemCount > 0) {
             if (mLastVisibleItemPosition == totalItemCount - 1) {
-                loadArticles();
+//                loadArticles();
             } else if (mFirstVisibleItemPosition == 0) {
                 // Scroll to top
             }
