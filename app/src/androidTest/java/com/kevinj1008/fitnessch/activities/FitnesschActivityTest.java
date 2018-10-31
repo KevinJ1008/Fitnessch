bb04f2d601f47611318fea5a9f3ebe438088bd4d
KevinJ1008 <car4353666@gmail.com>
2018-10-31 21:24:04 +0800

    Build v1.0.2 signed apk and refactor test util

--- app/src/androidTest/java/com/kevinj1008/fitnessch/activities/FitnesschActivityTest.java
+++ app/src/androidTest/java/com/kevinj1008/fitnessch/activities/FitnesschActivityTest.java
@@ -1,21 +1,14 @@
 package com.kevinj1008.fitnessch.activities;
 
 import android.os.SystemClock;
-import android.support.annotation.NonNull;
 import android.support.test.espresso.contrib.RecyclerViewActions;
-import android.support.test.espresso.matcher.BoundedMatcher;
 import android.support.test.rule.ActivityTestRule;
-import android.support.v7.widget.RecyclerView;
-import android.view.View;
 
 import com.kevinj1008.fitnessch.R;
 
-import org.hamcrest.Description;
-import org.hamcrest.Matcher;
 import org.junit.Rule;
 import org.junit.Test;
 
-import static android.support.test.espresso.Espresso.onData;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.pressBack;
@@ -23,11 +16,8 @@ import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.contrib.RecyclerViewActions.scrollToPosition;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.isRoot;
-import static android.support.test.espresso.matcher.ViewMatchers.withChild;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
-import static com.google.common.base.Preconditions.checkNotNull;
-import static org.hamcrest.core.AllOf.allOf;
-import static org.junit.Assert.*;
+import static com.kevinj1008.fitnessch.EspressoTestUtil.atPosition;
 
 public class FitnesschActivityTest {
 
@@ -51,24 +41,4 @@ public class FitnesschActivityTest {
                 .check(matches(atPosition(0, isDisplayed())));
     }
 
-    public static Matcher<View> atPosition(final int position, @NonNull final Matcher<View> itemMatcher) {
-        checkNotNull(itemMatcher);
-        return new BoundedMatcher<View, RecyclerView>(RecyclerView.class) {
-            @Override
-            public void describeTo(Description description) {
-                description.appendText("has item at position " + position + ": ");
-                itemMatcher.describeTo(description);
-            }
-
-            @Override
-            protected boolean matchesSafely(final RecyclerView view) {
-                RecyclerView.ViewHolder viewHolder = view.findViewHolderForAdapterPosition(position);
-                if (viewHolder == null) {
-                    // has no item on such position
-                    return false;
-                }
-                return itemMatcher.matches(viewHolder.itemView);
-            }
-        };
-    }
 }
